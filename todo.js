cc5d7fe96c7bc8a2faf5fff63512bd26e475d03f
Kemal Emre Erginkoç <keerginkoc@gmail.com>
2022-09-26 21:14:41 +0300

    Delete from storage

--- todo.js
+++ todo.js
@@ -41,12 +41,24 @@ function addTodo(e){
 function deleteTodo(e){
     
     if(e.target.className === "fa fa-remove"){
-        
+
         e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
         showAlert("success", "Todo Başarı ile Silindi")
     }
 
     
+}
+
+function deleteTodoFromStorage(deletetodo) {
+    let todos = getTodosFromStorage();
+    todos.forEach(function(todo, index){
+        if (todo === deletetodo){
+            todos.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function loadAllTodosToUI(){
